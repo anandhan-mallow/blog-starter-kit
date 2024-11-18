241f665154b4445f5a11484b550010f3c39785d4
Anandhan <anandhan@mallow-tech.com>
2024-11-18 13:55:43 +0530

    Create brandwell.tsx

---
+++ src/app/_components/brandwell.tsx
@@ -0,0 +1,21 @@
+export function Brandwell() {
+  return (
+    (function () {
+            var brandwell = window.brandwell || (window.brandwell = []);
+            if (brandwell.invoked) return;
+            brandwell.invoked = true;
+            brandwell.load = function (bwAppId) {
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.async = true;
+                script.src = "https://script.brandwell.ai/tracking.min.js";
+                var n = document.getElementsByTagName("script")[0];
+                n.parentNode.insertBefore(script, n);
+                brandwell.appId = bwAppId;
+            };
+            brandwell.load("brandwell-testing");
+        })();
+  );
+}
+
+export default Brandwell;
